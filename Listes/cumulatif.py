a7ca17f1c9895a2eab3a458edb313989695f0532
unknown <cecce@FRCLOC203-67392.ecolecatholique.ca>
2020-12-17 12:03:08 -0500

    chrismas

---
+++ Listes/cumulatif.py
@@ -0,0 +1,30 @@
+# Programer par: Nicholas Matta 
+# Le date: le 17 déc 2020
+# adress couriel: matnic02@ecolecatholique.ca 
+print("Run or quit")
+choix=input().lower()
+christmas=True 
+
+while christmas:
+    
+    liste = [1,5,10,15,20,25,30,35,40,45,50,55]
+    if choix=="run":
+        for i in liste:
+            #print ("voici le list ")
+            if i > 50:
+                    print("sa c'est des bon de 5")
+                    break
+            print(i)
+        print("")
+        v = "Passer un joyeuse noël est bon nouvel année 2021"
+        for lettre in v:
+            print(lettre)
+        print("")
+        i = 0
+        while i < 15:
+            print("🎅 🤶 🎄 🎁 👼  ")
+            i = i +1
+            christmas=False
+    elif choix=="quit":
+        christmas=False 
+        print("ok joyeux noël")
