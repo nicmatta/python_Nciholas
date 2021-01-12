4aa6d45f2be7b5855915a2ffd48d0b0e555a9a3d
Nicholas Matta <matnic02@ecolecatholique.ca>
2021-01-12 14:40:31 +0000

    nore exemple

--- pygame/pgzero_test.py
+++ pygame/pgzero_test.py
@@ -1,6 +1,22 @@
 import pgzrun
 
-def draw():
-    screen.fill("cyan")
 
-pgzrun.go()
+WIDTH  = 500	
+HEIGH = 300
+
+alien = Actor("alien.png")
+alien.pos = (0, HEIGHT / 2)
+speed = 2
+
+
+
+def draw():	
+    """ Tout ce qu'on veut voir dans notre fenêtre"""
+    screen.fill("blue")	    
+    alien.draw()
+
+def update():
+    alien.x = alien.x + speed
+
+
+pgzrun.go() 
