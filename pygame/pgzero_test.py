c25ceee8f4c037c1d80ef40b563bccd4ed1e344c
Nicholas Matta <matnic02@ecolecatholique.ca>
2021-01-12 15:04:13 +0000

    trouver erre dans code pygame

--- pygame/pgzero_test.py
+++ pygame/pgzero_test.py
@@ -1,22 +1,12 @@
 import pgzrun
-
-
 WIDTH  = 500	
 HEIGH = 300
-
 alien = Actor("alien.png")
-alien.pos = (0, HEIGHT / 2)
-speed = 2
-
-
-
+speed = 1
 def draw():	
     """ Tout ce qu'on veut voir dans notre fenêtre"""
     screen.fill("blue")	    
     alien.draw()
-
 def update():
     alien.x = alien.x + speed
-
-
 pgzrun.go() 
