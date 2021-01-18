7a689f88cc8baddea619e4211a83023269dba332
Nicholas Matta <matnic02@ecolecatholique.ca>
2021-01-18 17:53:23 +0000

    ajouté actor

--- pygame/pgzero_test.py
+++ pygame/pgzero_test.py
@@ -1,13 +1,22 @@
 import pgzrun
-WIDTH  = 500
-HEIGH = 300
+# je métre les dimision du fenatre 
+WIDTH  = 700
+HEIGH = 500
+# indentifi les actor
 alien = Actor("alien.png")
-speed = 0.5
+dog = Actor("2alien.png")
+# code les vitess 
+speed = 0.5 
+dogspeed=0.1
+# coleur du fenatre 
 def draw():	
     screen.fill("black")	    
     alien.draw()
+    dog.draw()
 def update():
     alien.x = alien.x + speed
+    dog.x=dog.x+dogspeed 
+# changement de angle 
 def on_mouse_move(pos):
     global p_text, a_text
     p_text = "Position: " + str(pos)
