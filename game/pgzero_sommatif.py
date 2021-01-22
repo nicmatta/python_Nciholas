684fd3832783cd5b6b0419e1f884085f79e8a113
Nicholas Matta <matnic02@ecolecatholique.ca>
2021-01-22 14:48:05 +0000

    ajouter les roche

--- game/pgzero_sommatif.py
+++ game/pgzero_sommatif.py
@@ -3,30 +3,40 @@ import random
 WIDTH, HEIGHT = 600, 280
 monnaie = Actor("dollar.png")
 vaisseau = Actor("startup.png")
+roche = Actor("rock.png")
 score=0
 monnaie.speed = 1.5
+roche.speed = 1
 game_over = False
 def reset():
-    monnaie.speed += 0
-    monnaie.x = 0
-    monnaie.y = random.randint(0, HEIGHT - monnaie.height)
+   roche.speed += 0
+   roche.x = 0
+   roche.y =random.randint(0,HEIGHT - roche.height)
+   monnaie.speed += 0
+   monnaie.x = 0
+   monnaie.y = random.randint(0, HEIGHT - monnaie.height)
 def draw():
   screen.clear()
   screen.draw.text("point: " + str(score), midtop = (WIDTH / 2, 5), fontsize = 40)
   if (not game_over):
     monnaie.draw()
     vaisseau.draw() 
+    roche.draw()
   else:
     screen.draw.text("Game Over", center = (WIDTH / 2,HEIGHT / 2), fontsize = 40)
 def update():
     global score, game_over
     if (monnaie.x < WIDTH):
         monnaie.x += monnaie.speed
+        roche.x += roche.speed 
     else:
         game_over = True
     if (vaisseau.colliderect(monnaie)):
         score += 1
         reset()
+    if (vaisseau.colliderect(roche)):
+      score -= 5
+      reset()
 def on_mouse_move(pos):
     vaisseau.pos = pos
 pgzrun.go() 
