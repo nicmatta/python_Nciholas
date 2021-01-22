8a6a3c549a07821aa311a007a9490c742c60d7d9
Nicholas Matta <matnic02@ecolecatholique.ca>
2021-01-22 04:08:58 +0000

    ajouter des photo

--- game/pgzero_sommatif.py
+++ game/pgzero_sommatif.py
@@ -1,29 +1,34 @@
 import pgzrun
-WIDTH, HEIGHT = 800, 600
-roche = Actor("rock.png")
+import random
+import sys
+WIDTH, HEIGHT = 600, 280
+monnaie = Actor("dollar.png")
 vaisseau = Actor("startup.png")
 score=0
-roche.speed = 0.5
+monnaie.speed = 1.5
 game_over = False
 def reset_faster():
-    roche.speed += 1
-    roche.x = 0
+    monnaie.speed += 0
+    monnaie.x = 0
+    monnaie.y = random.randint(0, HEIGHT - monnaie.height)
 def draw():
   screen.clear()
   screen.draw.text("Score: " + str(score), midtop = (WIDTH / 2, 5), fontsize = 40)
   if (not game_over):
-    roche.draw()
-    vaisseau.draw()
+    monnaie.draw()
+    vaisseau.draw() 
+
   else:
     screen.draw.text("Game Over", center = (WIDTH / 2,HEIGHT / 2), fontsize = 40)
 def update():
     global score, game_over
-    if (roche.x < WIDTH):
-        roche.x += roche.speed
+    if (monnaie.x < WIDTH):
+        monnaie.x += monnaie.speed
     else:
-        game_over = True 
-    if (vaisseau.colliderect(roche)):
+        game_over = True
+    if (vaisseau.colliderect(monnaie)):
         score += 1
+        reset_faster()
 def on_mouse_move(pos):
     vaisseau.pos = pos
 pgzrun.go() 
