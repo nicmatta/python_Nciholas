e1c9d27d7cb5755473528b8b989d1544b5e51ce0
Nicholas Matta <matnic02@ecolecatholique.ca>
2021-01-25 19:50:32 +0000

    added rocket

--- game/pgzero_sommatif.py
+++ game/pgzero_sommatif.py
@@ -4,9 +4,11 @@ WIDTH, HEIGHT = 600, 260
 monnaie = Actor("dollar.png")
 vaisseau = Actor("startup.png",)
 roche = Actor("rock (1).png")
+rocket = Actor("rocket (1).png")
 score=0
-monnaie.speed = 1.5
+monnaie.speed = 1.3
 roche.speed = 1
+rocket.speed = 0.7
 game_over = False
 def reset():
    roche.speed += 0
@@ -15,6 +17,9 @@ def reset():
    monnaie.speed += 0
    monnaie.x = 0
    monnaie.y = random.randint(0, HEIGHT - monnaie.height)
+   rocket.speed += 0
+   rocket.x = 0
+   rocket.y = random.randint(0, HEIGHT - rocket.height)
 def draw():
   screen.blit("bg",(0,0))
   screen.draw.text("point: " + str(score), midtop = (WIDTH / 2, 5), fontsize = 40)
@@ -22,6 +27,7 @@ def draw():
     monnaie.draw()
     vaisseau.draw() 
     roche.draw()
+    rocket.draw()
   else:
     screen.draw.text("Game Over", center = (WIDTH / 2,HEIGHT / 2), fontsize = 40)
 def update():
@@ -29,6 +35,7 @@ def update():
     if (monnaie.x < WIDTH):
         monnaie.x += monnaie.speed
         roche.x += roche.speed 
+        rocket.x += rocket.speed
     else:
         game_over = True
     if (vaisseau.colliderect(monnaie)):
@@ -37,6 +44,8 @@ def update():
     if (vaisseau.colliderect(roche)):
       score -= 2
       reset()
+    if (vaisseau.colliderect(rocket)):
+      game_over= True 
 def on_mouse_move(pos):
     vaisseau.pos = pos
 pgzrun.go() 
--- game/readme.md
+++ game/readme.md
@@ -2,5 +2,6 @@ instruction:
 frappée une roche est perdu 2 points<
 collecter les soues est gagnée 1 point
 si tu attrapes pas tous les soues "gamme over"
+si tu frappe le fusée "gamme over"
 tu bouge le vaisseau avec le souri 
 essayer de attraper le plus de soue possible
